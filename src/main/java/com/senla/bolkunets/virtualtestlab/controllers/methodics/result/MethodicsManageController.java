93fbe2cca96356fdf9bfe1a2d6c3fe6067915d04
Andrei_vishneuski <andrei_vishneuski@senla.eu>
2018-06-18 15:12:55 +0300

    фыв

--- src/main/java/com/senla/bolkunets/virtualtestlab/controllers/methodics/result/MethodicsManageController.java
+++ src/main/java/com/senla/bolkunets/virtualtestlab/controllers/methodics/result/MethodicsManageController.java
@@ -2,6 +2,7 @@ package com.senla.bolkunets.virtualtestlab.controllers.methodics.result;
 
 import com.senla.bolkunets.virtualtestlab.controllers.dto.messages.ResponseMessageDto;
 import com.senla.bolkunets.virtualtestlab.domain.services.MethodicsService;
+import com.senla.bolkunets.virtualtestlab.domain.services.PassingFactService;
 import org.dozer.DozerBeanMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,15 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/methodics/admin/manage")
 public class MethodicsManageController {
 
+    private PassingFactService passingFactService;
+
     private MethodicsService methodicsService;
 
     private DozerBeanMapper dozerBeanMapper;
 
-    public MethodicsManageController(MethodicsService methodicsService, DozerBeanMapper dozerBeanMapper) {
+    public MethodicsManageController(PassingFactService passingFactService, MethodicsService methodicsService,
+            DozerBeanMapper dozerBeanMapper) {
         this.methodicsService = methodicsService;
         this.dozerBeanMapper = dozerBeanMapper;
+        this.passingFactService = passingFactService;
     }
 
+    @RequestMapping(value = "/delete", method = RequestMethod.PUT)
+    public ResponseMessageDto create(@RequestParam(name= "userId") Integer userId, @RequestParam(name= "methodicsId") Integer methodicsId){
+        passingFactService.deletePassingFactById(userId, methodicsId);
+        return dozerBeanMapper.map("passing fact was deleted", ResponseMessageDto.class);
+    }
 
     @RequestMapping(value = "/open", method = RequestMethod.GET)
     public ResponseMessageDto openForUser( @RequestParam(value="userId") Integer userId, @RequestParam(value="methodicsId") Integer methodicsId)
