59bbe7708cd3c4830496a549506f3645eae4bf3c
Andrei_vishneuski <andrei_vishneuski@senla.eu>
2018-06-18 16:56:24 +0300

    added repository

--- src/main/java/com/senla/bolkunets/virtualtestlab/domain/dao/impl/ContactDaoImpl.java
+++ src/main/java/com/senla/bolkunets/virtualtestlab/domain/dao/impl/ContactDaoImpl.java
@@ -2,7 +2,9 @@ package com.senla.bolkunets.virtualtestlab.domain.dao.impl;
 
 import com.senla.bolkunets.virtualtestlab.domain.dao.info.ContactDao;
 import com.senla.bolkunets.virtualtestlab.domain.model.info.Contact;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ContactDaoImpl extends AbstractDao<Integer, Contact> implements ContactDao {
 	public ContactDaoImpl() {
 		super(Contact.class);
--- src/main/java/com/senla/bolkunets/virtualtestlab/domain/dao/impl/DocumentDaoImpl.java
+++ src/main/java/com/senla/bolkunets/virtualtestlab/domain/dao/impl/DocumentDaoImpl.java
@@ -2,7 +2,9 @@ package com.senla.bolkunets.virtualtestlab.domain.dao.impl;
 
 import com.senla.bolkunets.virtualtestlab.domain.dao.info.DocumentDao;
 import com.senla.bolkunets.virtualtestlab.domain.model.info.Document;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class DocumentDaoImpl extends AbstractDao<Integer, Document> implements DocumentDao {
 	public DocumentDaoImpl(Class<Document> type) {
 		super(type);
